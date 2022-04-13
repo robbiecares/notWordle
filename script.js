7fee943faed224fa5989b60d1f92e18d70935665
robbiecares <robbiecares@gmail.com>
2022-04-13 17:14:45 +0200

    disabled touch events for testing of double tap to zoom

--- script.js
+++ script.js
@@ -178,25 +178,25 @@ const displayController = (() => {
         }
     }
 
-    // this will prevent any zoom action by the user in ios safari and also prevent the "zoom to tabs" feature:
-        // https://stackoverflow.com/questions/11689353/disable-pinch-zoom-on-mobile-web
-    document.addEventListener('gesturestart', function(e) {
-        e.preventDefault();
-        // special hack to prevent zoom-to-tabs gesture in safari
-        document.body.style.zoom = 0.99;
-    });
+    // // this will prevent any zoom action by the user in ios safari and also prevent the "zoom to tabs" feature:
+    //     // https://stackoverflow.com/questions/11689353/disable-pinch-zoom-on-mobile-web
+    // document.addEventListener('gesturestart', function(e) {
+    //     e.preventDefault();
+    //     // special hack to prevent zoom-to-tabs gesture in safari
+    //     document.body.style.zoom = 0.99;
+    // });
     
-    document.addEventListener('gesturechange', function(e) {
-        e.preventDefault();
-        // special hack to prevent zoom-to-tabs gesture in safari
-        document.body.style.zoom = 0.99;
-    });
+    // document.addEventListener('gesturechange', function(e) {
+    //     e.preventDefault();
+    //     // special hack to prevent zoom-to-tabs gesture in safari
+    //     document.body.style.zoom = 0.99;
+    // });
     
-    document.addEventListener('gestureend', function(e) {
-        e.preventDefault();
-        // special hack to prevent zoom-to-tabs gesture in safari
-        document.body.style.zoom = 0.99;
-    });
+    // document.addEventListener('gestureend', function(e) {
+    //     e.preventDefault();
+    //     // special hack to prevent zoom-to-tabs gesture in safari
+    //     document.body.style.zoom = 0.99;
+    // });
 
     let letters;
     let guessTiles;
